f7678d8374160ea5677bdeca6cec561809214bce
Ayush Dhar <108949872+AyushDhar05@users.noreply.github.com>
2024-10-31 20:54:04 -0500

    ProductService initial commit

---
+++ ProductService/src/test/java/me/ayushdhar/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ayushdhar.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
